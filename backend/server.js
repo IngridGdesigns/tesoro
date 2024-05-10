20655f2ed5abf54c759720e1e1a9983c59036ea7
IngridGdesigns <garciai@protonmail.com>
2024-05-10 02:54:55 -0700

    added auth0/jwt but not in use yet

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ const cors = require('cors') //cors
 const morgan = require('morgan');//HTTP request logger middleware, generates logs for API request
 const app = express();
 
+// const jwtCheck = auth({
+//   audience: 'https://tesoroAPI',
+//   issuerBaseURL: 'https://createmagic.us.auth0.com/',
+//   tokenSigningAlg: 'RS256'
+// });
+
 const PORT = process.env.PORT || 3005;
 
 app.use(express.json())
@@ -13,9 +19,10 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cors()); 
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   // res.send({ "msg": "This has CORS enabled 🎈" });
   // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
@@ -47,7 +54,40 @@ app.use('/api/goals', goalRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/transactions', transactionRoutes);
 
+// app.get('/authorized', jwtCheck, function (req, res) {
+//     res.send('Secured Resource');
+// });
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
+
+
+// exports.onExecutePostLogin = async (event, api) => {
+//   // Check if the user has a role assigned
+//   if (event.authorization && event.authorization.roles && event.authorization.roles.length > 0) {
+//     return;
+//   }
+
+
+
+//   make every user a user
+//   // Create management API client instance
+//   const ManagementClient = require("auth0").ManagementClient;
+
+//   const management = new ManagementClient({
+//     domain: event.secrets.DOMAIN,
+//     clientId: event.secrets.CLIENT_ID,
+//     clientSecret: event.secrets.CLIENT_SECRET,
+//     audience: event.secrets.AUDIENCE,
+//   });
+  
+//   const params =  { id : event.user.user_id };
+//   const data = { "roles" : ['rol_D8qPmzK7DTR9OKPS'] };
+
+//   try {
+//     await management.users.assignRoles(params, data);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
