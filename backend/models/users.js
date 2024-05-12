da719903c40a18236742df01bb12880605931fb6
IngridGdesigns <garciai@protonmail.com>
2024-05-12 12:16:39 -0700

    included user_sub - Auth0 user id into queries WIP

--- backend/models/users.js
+++ backend/models/users.js
@@ -42,14 +42,17 @@ const getUserById = async (req, res) => {
 
 const createUser = async (req, res) => {
   const client = await pool.connect();
-// curl --data "name=Uriel&username=cloudyday&laguna12@hotmail.com&password=12345" https://localhost:3005/api/users
-  const { name, username, email, password } = req.body;
+  const { name, username, email, password, user_sub } = req.body;
 
-  await client.query('INSERT INTO USERS(name, username, email, password) VALUES ($1, $2, $3, $4) RETURNING *', 
-    [name, username, email, password], (err, results) => {
+  await client.query('INSERT INTO users(name, username, email, password, user_sub) VALUES ($1, $2, $3, $4, $5) RETURNING *', 
+    [name, username, email, password, user_sub], (err, results) => {
       if (err) {
-          res.status(500).send('Server error')
-          client.release()
+        if (err.code === '23505') { // Unique constraint violation
+          res.status(400).send('Email or username already exists');
+        } else {
+          res.status(500).send('Server error');
+        }
+        client.release();
       } else {
           res.status(200).json(results.rows[0]);
           client.release()
@@ -61,7 +64,7 @@ const createUser = async (req, res) => {
 const editUserById = async (req, res) => {
   const client = await pool.connect();
 
-  const { user_id, username, name, email, password } = req.body;
+  const { username, name, email, password, user_id, user_sub } = req.body;
 
   await client.query(`SET 
     username = CASE 
@@ -80,7 +83,7 @@ const editUserById = async (req, res) => {
                    WHEN $4 != '' THEN $4 
                    ELSE password 
                END
-    WHERE user_id = $5 RETURNING *`, [username, name, email, password, user_id], (err, results) => {
+    WHERE user_id = $5 AND user_sub = $6 RETURNING *`, [username, name, email, password, user_id, user_sub], (err, results) => {
     if (err) {
       console.log('Oh noes you have an error!!')
       res.status(500).send('There is a server error')
@@ -100,7 +103,7 @@ const editUserRole = async (req, res) => {
   const { user_id, role} = req.body;
 
   await client.query(`UPDATE users SET role = $1 
-  WHERE user_id = $2`, [role, user_id], (err, results) => {
+  WHERE user_id = $2 AND user_sub = $1`, [role[0], user_id, user_sub], (err, results) => {
     if (err) {
       console.log('Oh noes you have an error!!')
       res.status(500).send('There is a server error')
@@ -118,7 +121,7 @@ const deleteUser = async (req, res) => {
   const client = pool.connect();
   const user_id = req.params.user_id;
 
-  (await client).query('DELETE FROM users WHERE user_id = $1', [user_id], (err, results) => {
+  (await client).query('DELETE FROM users WHERE user_id = $1 AND user_sub = $2', [user_id, user_sub], (err, results) => {
     if (err) {
       res.status(500).send(`Oh noes a Servor error: ${err}!`)
       client.release()
