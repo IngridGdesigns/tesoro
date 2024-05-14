8aa8905f8a19e1bf27258652f7eb305f42af2860
IngridGdesigns <garciai@protonmail.com>
2024-05-14 02:02:18 -0700

    added pages

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,7 +4,10 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const UserProfile = () => {
     const { user, isAuthenticated, isLoading } = useAuth0();
-    console.log(user.name)
+    // console.log(user.name)
+    if (!user) {
+        return null;
+    }
 
     // if (user.assignedRoles !== 'Admin') {
     //     console.log('not admin')
