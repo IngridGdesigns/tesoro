ac48ece7dc662ab37ec02364986a61bb777b24ee
IngridGdesigns <garciai@protonmail.com>
2024-05-14 02:04:59 -0700

    cleanup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,11 @@
-// import React, { createRoot } from 'react'
+// eslint-disable-next-line no-unused-vars
+import React, { createRoot } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Auth0Provider } from '@auth0/auth0-react';
-// import { useRedirectCallback } from "./Auth0ProviderWithNavigate"
+
+
+
 
 const domain = import.meta.env.VITE_REACT_APP_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_REACT_APP_AUTH0_CLIENT_ID;
@@ -15,6 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     authorizationParams={{
       redirect_uri: window.Location.origin || redirectUri,
     }}
+
   >
     <App/>
   </Auth0Provider>
